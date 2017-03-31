4005a32a149b84ec9b80e94b83b91b32f9c2a4f2
Alice Easter <alice.easter@mymail.champlain.edu>
2017-03-31 17:53:37 -0400

    Syntax is key

--- Server/routes/api.js
+++ Server/routes/api.js
@@ -11,7 +11,7 @@ router.get('/tasks', function(req, res, next) {
 
 router.post('/addTask', function(req, res, next) {
     var task = req.body;
-    if (task.uid != undefined) { // assume task is valid
+    if (task.uid !== undefined) { // assume task is valid
         taskManager.addTask(task);
     }
     res.send("OK");
