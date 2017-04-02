db3c06be8645959cc9c3021206e768e4d6fec43d
Alice Easter <alice.easter@mymail.champlain.edu>
2017-04-02 14:41:00 -0400

    corrected variable insertion

--- MotionSensor/motionSensor.py
+++ MotionSensor/motionSensor.py
@@ -7,4 +7,4 @@ from gpiozero import MotionSensor
 pir = MotionSensor(27)
 while True:
     if pir.motion_detected:
-        r = requests.post("http://codewonderland.me:6789/api/addTask", data={'message':'You still have errands to complete!', 'author':'Errands_Bot', 'timeCreated':'\datetime.now()', 'uid':'\uuid.uuid4()'})
+        r = requests.post("http://codewonderland.me:6789/api/addTask", data={'message':'You still have errands to complete!', 'author':'Errands_Bot', 'timeCreated':"'" + str(datetime.datetime.now()) + "'", 'uid':"'" + str(uuid.uuid4()) + "'"})
