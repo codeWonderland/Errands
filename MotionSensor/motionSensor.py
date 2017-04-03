6fd1db4179b0c26055b85ee4538c67ed47566666
Alice Easter <alice.easter@mymail.champlain.edu>
2017-04-03 16:00:07 -0400

    added sleep in motion sensor to avoid… insanity

--- MotionSensor/motionSensor.py
+++ MotionSensor/motionSensor.py
@@ -1,10 +1,11 @@
-import os
 import requests
 import datetime
 import uuid
+import time
 from gpiozero import MotionSensor
 
 pir = MotionSensor(27)
 while True:
     if pir.motion_detected:
         r = requests.post("http://codewonderland.me:6789/api/addTask", data={'message':'You still have errands to complete!', 'author':'Errands_Bot', 'timeCreated':"'" + str(datetime.datetime.now()) + "'", 'uid':"'" + str(uuid.uuid4()) + "'"})
+        time.sleep(2)
