be89b811cc9b6e74ba281fa26da50c9592cd730e
Alice Easter <alice.easter@mymail.champlain.edu>
2017-04-03 16:06:52 -0400

    corrected formatting issue with motion sensor

--- MotionSensor/motionSensor.py
+++ MotionSensor/motionSensor.py
@@ -7,5 +7,5 @@ from gpiozero import MotionSensor
 pir = MotionSensor(27)
 while True:
     if pir.motion_detected:
-        r = requests.post("http://codewonderland.me:6789/api/addTask", data={'message':'You still have errands to complete!', 'author':'Errands_Bot', 'timeCreated':"'" + str(datetime.datetime.now()) + "'", 'uid':"'" + str(uuid.uuid4()) + "'"})
+        r = requests.post("http://codewonderland.me:6789/api/addTask", data={'message':'You still have errands to complete!', 'author':'Errands_Bot', 'timeCreated': str(datetime.datetime.now()), 'uid': str(uuid.uuid4())})
         time.sleep(2)
